e6f9e6bb74b021c6b59e0c00b2d8ce70fb7e07ef
Akshat Agrawal <49070489+akshatagrawal9874@users.noreply.github.com>
2021-10-23 12:04:05 +0530

    Add files via upload

---
+++ assets/images/index.js
@@ -0,0 +1,12 @@
+export const bascket = require("./bascket.png");
+export const fruitbascket = require("./fruitbascket.png");
+export const berry = require("./berry.png");
+export const breakfast = require("./breakfast.png");
+export const glowing = require("./glowing.png");
+export const peach = require("./peach.png");
+export const tropical = require("./tropical.png");
+export const drops = require("./drops.png");
+export const note = require("./note.png");
+export const deliverybike = require("./deliverybike.png");
+export const map = require("./map.png");
+export const order = require("./order.png");
